bf2caf5ad5d68f55989bf9002c3a09b4f8bb46aa
Anurag Band <anuragband17.com>
2026-06-02 19:11:54 -0400

    Week 1 Day 1 completed

---
+++ week1/day1/alert_generator.py
@@ -0,0 +1,8 @@
+username = "root"
+source_ip = "10.10.10.15"
+country = "Russia"
+
+print("ALERT")
+print("User:", username)
+print("IP:", source_ip)
+print("Country:", country)
---
+++ week1/day1/week1_day1.py
@@ -0,0 +1,7 @@
+username = "admin"
+source_ip = "192.168.1.10"
+failed_attempts = 7
+
+print("Username:", username)
+print("Source IP:", source_ip)
+print("Failed Attempts:", failed_attempts)
